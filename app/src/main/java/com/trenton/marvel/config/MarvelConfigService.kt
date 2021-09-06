a6fa81771a82c5eaeb063c971a69769900236ac4
ddukesterman <ddukesterman@gmail.com>
2021-09-06 09:43:14 -0500

    Removed API Key

--- app/src/main/java/com/trenton/marvel/config/MarvelConfigService.kt
+++ app/src/main/java/com/trenton/marvel/config/MarvelConfigService.kt
@@ -1,17 +1,13 @@
 package com.trenton.marvel.config
 
-import android.content.Context
-import dagger.hilt.android.qualifiers.ApplicationContext
 import javax.inject.Inject
 
 
 class MarvelConfigService
 @Inject
-constructor(
-    @ApplicationContext context: Context
-) {
+constructor() {
 
-    val publicKey = "059912624a9eacc16ba39d01484cc4f0"
-    val privateKey = "67c762fb6735763190431187b9733e6f76744942"
+    val publicKey = ""
+    val privateKey = ""
 
 }
